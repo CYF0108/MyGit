6a82b84c4ace5568b4db9f66bf93b2410ed9e7aa
成映飞 <1548052469@qq.com>
2019-12-09 16:08:31 +0800

    上传测试

---
+++ test_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test_1
+{
+    public class Caculate
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Min(double x, double y)
+        {
+            return x - y;
+        }
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+
+}
